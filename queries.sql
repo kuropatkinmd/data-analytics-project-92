d5fdc6d9a2600039d4dfcf8563eb2251e94a9149
kuropatkinmd <123442956+kuropatkinmd@users.noreply.github.com>
2024-03-18 20:57:33 +0400

    Add files via upload

--- queries.sql
+++ queries.sql
@@ -1,134 +1,142 @@
 --Данный запрос подсчитывает общее количество покупателей из таблицы customers
-SELECT
-	COUNT (*) AS customers_count
+SELECT COUNT(*) AS customers_count
 FROM
-	customers;
+    customers;
 
---Данный запрос определяет десятку лучших продавцов по суммарной выручке. Также содержит информацию о количестве операций каждого из них.
+/*Данный запрос определяет десятку лучших продавцов по суммарной выручке.
+Также содержит информацию о количестве операций каждого из них.*/
 SELECT
-	first_name || ' ' || last_name AS seller,
-	COUNT (sales_person_id) AS operations,
-	FLOOR(SUM(s.quantity * p.price)) AS income
+    e.first_name || ' ' || e.last_name AS seller,
+    COUNT(s.sales_person_id) AS operations,
+    FLOOR(SUM(s.quantity * p.price)) AS income
 FROM
-	employees e
-LEFT JOIN sales s
-ON
-	e.employee_id = s.sales_person_id
-LEFT JOIN products p 
-ON
-	s.product_id = p.product_id
+    employees AS e
+LEFT JOIN sales AS s
+    ON
+        e.employee_id = s.sales_person_id
+LEFT JOIN products AS p
+    ON
+        s.product_id = p.product_id
 GROUP BY
-	1
+    seller
 ORDER BY
-	income DESC NULLS LAST
+    income DESC NULLS LAST
 LIMIT 10;
 
---Данный содержит информацию о продавцах, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам.
---Таблица отсортирована по выручке по возрастанию.
+/*Данный содержит информацию о продавцах,
+чья средняя выручка за сделку меньше средней 
+выручки за сделку по всем продавцам.
+Таблица отсортирована по выручке по возрастанию.*/
 SELECT
-	first_name || ' ' || last_name AS seller,
-	FLOOR(AVG(s.quantity * p.price)) AS average_income
+    e.first_name || ' ' || e.last_name AS seller,
+    FLOOR(AVG(s.quantity * p.price)) AS average_income
 FROM
-	employees e
-LEFT JOIN sales s
-ON
-	e.employee_id = s.sales_person_id
-LEFT JOIN products p 
-ON
-	s.product_id = p.product_id
+    employees AS e
+LEFT JOIN sales AS s
+    ON
+        e.employee_id = s.sales_person_id
+LEFT JOIN products AS p
+    ON
+        s.product_id = p.product_id
 GROUP BY
-	1
+    seller
 HAVING
-	FLOOR(AVG(s.quantity * p.price))<(
-	SELECT
-		FLOOR(AVG(s.quantity * p.price))
-		FROM
-	sales s
-	LEFT JOIN products p 
-ON
-		s.product_id = p.product_id)
+    FLOOR(AVG(s.quantity * p.price)) < (
+        SELECT FLOOR(AVG(s.quantity * p.price))
+        FROM
+            sales AS s
+        LEFT JOIN products AS p
+            ON
+                s.product_id = p.product_id
+    )
 ORDER BY
-	average_income ASC;
+    average_income ASC;
 
---Данный запрос предостовляет информацию по суммарной выручке по дням недели по каждому продавцу.
+/*Данный запрос предостовляет информацию по
+суммарной выручке по дням недели по каждому продавцу.*/
 WITH tab AS (
-SELECT
-	e.first_name || ' ' || e.last_name as seller,
-	TO_CHAR(s.sale_date, 'Day') as day_of_week,
-	EXTRACT(ISODOW FROM	s.sale_date) AS dow,
-	FLOOR(SUM(s.quantity * p.price)) as income
-FROM
-	employees e
-INNER JOIN sales s
-ON
-	e.employee_id = s.sales_person_id
-LEFT JOIN products p 
-ON
-	s.product_id = p.product_id
-GROUP BY
-	1, 2, 3
-	)
-	SELECT
-	seller,
-	lower(day_of_week) AS day_of_week,
-	income
-FROM
-	tab
-ORDER BY
-	dow,
-	seller;
+    SELECT
+        e.first_name || ' ' || e.last_name AS seller,
+        TO_CHAR(s.sale_date, 'Day') AS day_of_week,
+        EXTRACT(ISODOW FROM s.sale_date) AS dow,
+        FLOOR(SUM(s.quantity * p.price)) AS income
+    FROM
+        employees AS e
+    INNER JOIN sales AS s
+        ON
+            e.employee_id = s.sales_person_id
+    LEFT JOIN products AS p
+        ON
+            s.product_id = p.product_id
+    GROUP BY
+        seller, day_of_week, dow
+)
 
---Данный запрос выводит количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+. Сортировка по возрастным группам
 SELECT
-	CASE
-		WHEN age BETWEEN 16 AND 25 THEN '16-25'
-		WHEN age BETWEEN 26 AND 40 THEN '26-40'
-		WHEN age > 40 THEN '40+'
-	END AS age_category,
-	COUNT(*) AS age_count
+    seller,
+    income,
+    LOWER(day_of_week) AS day_of_week
 FROM
-	customers
-GROUP BY
-	age_category
+    tab
 ORDER BY
-	age_category;
+    dow,
+    seller;
 
---Данный запрос выводит данные по количеству уникальных покупателей и выручке за месяц. Сортировка по дате по возрастанию.
+/*Данный запрос выводит количество покупателей
+в разных возрастных группах: 16-25, 26-40 и 40+.
+Сортировка по возрастным группам*/
+SELECT
+    CASE
+        WHEN age BETWEEN 16 AND 25 THEN '16-25'
+        WHEN age BETWEEN 26 AND 40 THEN '26-40'
+        WHEN age > 40 THEN '40+'
+    END AS age_category,
+    COUNT(*) AS age_count
+FROM
+    customers
+GROUP BY
+    age_category
+ORDER BY
+    age_category;
+
+/*Данный запрос выводит данные по количеству уникальных покупателей и выручке за месяц. 
+Сортировка по дате по возрастанию.*/
 select
-	to_char(sale_date,'YYYY-MM') as selling_month,
-	count (distinct customer_id) as total_customers,
-	FLOOR(SUM(s.quantity * p.price)) as income
+    to_char(s.sale_date, 'YYYY-MM') as selling_month,
+    count(distinct s.customer_id) as total_customers,
+    floor(sum(s.quantity * p.price)) as income
 from
-	sales s
-join products p
-on
-		s.product_id = p.product_id
+    sales as s
+inner join products as p
+    on
+        s.product_id = p.product_id
 group by
-	1
+    selling_month
 order by
-	selling_month;
+    selling_month;
 
---Данный запрос предоставляет отчет о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0) с данными о дате и продавце. 
---Сортировка по id.
-select
-	distinct on
-	(c.customer_id)
+/*Данный запрос предоставляет отчет о покупателях, первая покупка которых была
+ * в ходе проведения акций (акционные товары отпускали
+ * со стоимостью равной 0) с данными о дате и продавце.
+ * Сортировка по id покупателя.*/
+select distinct on
+(c.customer_id)
+    s.sale_date,
     c.first_name || ' ' || c.last_name as customer,
-	sale_date,
-	e.first_name || ' ' || e.last_name as seller
+    e.first_name || ' ' || e.last_name as seller
 from
-	sales s
-left join customers c 
-on
-	s.customer_id = c.customer_id
-left join employees e
-on
-	s.sales_person_id = e.employee_id
-left join products p
-on
-	s.product_id = p.product_id
+    sales as s
+left join customers as c
+    on
+        s.customer_id = c.customer_id
+left join employees as e
+    on
+        s.sales_person_id = e.employee_id
+left join products as p
+    on
+        s.product_id = p.product_id
 where
-	p.price = 0
+    p.price = 0
 order by
-	c.customer_id,
-	sale_date;
+    c.customer_id,
+    s.sale_date;
